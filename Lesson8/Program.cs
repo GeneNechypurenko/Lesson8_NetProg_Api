7971bc64691ab3a167095ec153e5ea42c56594ee
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-03-22 19:10:16 +0200

    Add project files.

---
+++ Lesson8/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public class Exchange
+    {
+        public string Ccy { get; set; }
+        public string BaseCcy { get; set; }
+        public decimal Buy { get; set; }
+        public decimal Sale { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string apiUrl = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                JArray exchanges = JsonConvert.DeserializeObject<JArray>(jsonResponse);
+
+                foreach (var exchange in exchanges)
+                {
+                    Console.WriteLine(exchange.ToString());
+                }
+            }
+        }
+    }
+}
